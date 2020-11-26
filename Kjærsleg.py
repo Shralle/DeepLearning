63c48a939647676d5e02baf124741ad5a42ffafb
Fredferik Kjaer <f.kjaer@icloud.com>
2020-11-26 09:22:24 +0100

    Documentation

--- Kjærsleg.py
+++ Kjærsleg.py
@@ -13,22 +13,29 @@ import torch.nn as nn
 import numpy as np
 import torch.from_numpy as from_numpy
 import torch.optim as optim
+#Initialize ARRAYS
 inputs = np.ndarray(shape=(18,3,256,256), dtype=float)
 labels = np.ndarray(shape=(18,10,256,256), dtype=float)
+#Set directory for data
 data_dir = 'carseg_data/save'
+#Initialize counter
 i=0
+#Loop for loading data
 for filename in os.listdir(data_dir):
     datafiles = os.listdir(data_dir)
     if filename.endswith('.npy'):
         traindata = np.load(data_dir+'/' + filename)
         inputs[i] = traindata[0:3]
         labels[i] = traindata[3:13]
-    i = i + 1        
+    i = i + 1       
+#Convert data from nNumpy arrays into tensors 
 inputs = torch.from_numpy(inputs)
 labels = torch.from_numpy(labels)
+#Initialize convolution layer size
 in_channels = 256
 mid_channels = 256
 out_channels = 256
+#Initialize Network
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True):
         super(UNet, self).__init__()
@@ -42,11 +49,15 @@ class UNet(nn.Module):
                 )
     def forward(self, x):
         return self.double_conv(x)
+#Create Network
 net = UNet(n_channels = 256,n_classes = 9)
+#Ved ikke helt hvad det her gør, har bare brugt det før i tidligere ogpgaver :/
 trainloader = torch.utils.data.DataLoader(inputs, batch_size=1, shuffle=True, num_workers=2)
 batch = next(iter(trainloader))
+#Optimizer / loss function
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001, weight_decay=1e-4)
+#Training
 num_epoch = 3
 for epoch in range(num_epoch):  # loop over the dataset multiple times
     running_loss = 0.0
