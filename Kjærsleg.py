c8022aef2dbe57abfdc5bb55298f6a8d5861b07a
Fredferik Kjaer <f.kjaer@icloud.com>
2020-11-30 15:24:28 +0100

    Mere til opsætning af data

--- Kjærsleg.py
+++ Kjærsleg.py
@@ -14,6 +14,7 @@ import numpy as np
 #import torch.from_numpy as from_numpy
 import torch.optim as optim
 #Initialize ARRAYS
+train = np.ndarray(shape=(18,13,256,256), dtype = float)
 inputs = np.ndarray(shape=(18,3,256,256), dtype=float)
 labels = np.ndarray(shape=(18,10,256,256), dtype=float)
 #Set directory for data
@@ -26,12 +27,15 @@ for filename in os.listdir(data_dir):
     datafiles = os.listdir(data_dir)
     if filename.endswith('.npy'):
         traindata = np.load(data_dir+'/' + filename)
-        inputs[i] = traindata[0:3]
-        labels[i] = traindata[3:13]
+        train[i] = traindata
+#        inputs[i] = traindata[0:3]
+#        labels[i] = traindata[3:13]
     i = i + 1       
 #Convert data from nNumpy arrays into tensors 
-inputs = torch.from_numpy(inputs).float()
-labels = torch.from_numpy(labels).float()
+
+train = torch.from_numpy(train).float()
+#inputs = torch.from_numpy(inputs).float()
+#labels = torch.from_numpy(labels).float()
 #Initialize convolution layer size
 in_channels = 3
 mid_channels = 256
@@ -51,15 +55,17 @@ class UNet(nn.Module):
     def forward(self, x):
         return self.double_conv(x)
 #Create Network
-net = UNet(n_channels = 256,n_classes = 9)
+net = UNet(n_channels = 3,n_classes = 9)
 #Ved ikke helt hvad det her gør, har bare brugt det før i tidligere ogpgaver :/
-trainloader = torch.utils.data.DataLoader(inputs, batch_size=1, shuffle=True, num_workers=0)
-batch = next(iter(trainloader))
-labelloader = torch.utils.data.DataLoader(labels, batch_size=1, shuffle=True, num_workers=0)
+trainloader = torch.utils.data.DataLoader(train, batch_size = 6, shuffle=True)
+train_data_iter = next(iter(trainloader))
+#labelsloader = torch.utils.data.DataLoader(labels, shuffle=True)
+
 #Optimizer / loss function
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001, weight_decay=1e-4)
 #Training
+from torch.autograd import Variable
 num_epoch = 3
 for epoch in range(num_epoch):  # loop over the dataset multiple times
     running_loss = 0.0
@@ -69,8 +75,12 @@ for epoch in range(num_epoch):  # loop over the dataset multiple times
         # wrap them in Variable
         # zero the parameter gradients
         # Your code here!
+        inputs = data[:,0:3,:,:]
+        labels = data[:,3:13,:,:]
+        inputs, labels = Variable(inputs), Variable(labels)
         optimizer.zero_grad()
-        targets = labelloader
+        targets = labels
+        targets = np.squeeze(targets, axis = 1)
         outputs = net(inputs)
         loss = criterion(outputs, targets)
         loss.forward()
