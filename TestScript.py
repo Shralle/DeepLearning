db55a55019ab1f858f5f5cfd19b0988d48d51320
SHRALLE <rasmusnissendahl@gmail.com>
2021-01-03 13:09:11 +0100

    Lads

--- TestScript.py
+++ TestScript.py
@@ -37,15 +37,21 @@ test_loader = torch.utils.data.DataLoader(test, batch_size = batch_size, shuffle
 
 PATH = './model/model_40epoch5batchV3.pt'
 net = UNet(n_channels = 3,n_classes = 9)
-if(torch == True):    
-    net = torch.load(PATH)
-if(torch == False):
-    net = torch.load(PATH, map_location=torch.device('cpu)'))
+
+use_cuda = torch.cuda.is_available()
+print("Running GPU.") if use_cuda else print("No GPU available.")
+if use_cuda:
+    net.cuda()
+#print(net)
+
+net = torch.load(PATH)
+
 #model.eval()
 
 correct = 0
 total = 0
 predictedlist = torch.zeros([len(test_loader), 256, 256], dtype=torch.float64)
+labellist = torch.zeros([len(test_loader), 256, 256], dtype=torch.float64)
 count = 0
 for data in test_loader:
     inputs = data[:,0:3,:,:]
@@ -62,6 +68,7 @@ for data in test_loader:
     #correct += (predicted == labels).sum()
     correct += torch.sum(predicted == labels)
     predictedlist[count, :, :] = predicted
+    labellist[count, :, :] = labels
     count = count + 1
 #print('Accuracy of the network on the {} test images: {:4.2f} %'.format(n_test, (100 * correct.true_divide(total*256*256))))
 print('Accuracy of the network on the {} test images: {:4.2f} %'.format(n_test, (100 * correct.true_divide(total*256*256))))
@@ -76,7 +83,8 @@ colors = {0: [int(0), int(0), int(0)],
           7: [int(150), int(10), int(150)],
           8: [int(10), int(250), int(10)]}
 print(predictedlist.shape)
-picture = predictedlist[1,:,:]
+#picture = predictedlist[0,:,:]
+picture = labellist[0,:,:]
 pictureprint = np.zeros((256,256,3),dtype=int)
 for i in range(256):
     for j in range(256):
