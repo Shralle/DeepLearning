091bc06b47f100abcdfb5c7f377386693d1234ee
SHRALLE <rasmusnissendahl@gmail.com>
2021-01-04 13:26:39 +0100

    changes

--- SoftDiceloss.py
+++ SoftDiceloss.py
@@ -1,19 +1,33 @@
 import torch
-import torch.nn as nn
-import numpy as np
-import torch.optim as optim
 
-def SoftDiceloss(prediction, target, mask):
-    batch_size = mask.shape[0]
-    mask = torch.abs(mask-1)
-    mask = mask.expand(9,batch_size,256,256)
-    mask = mask.permute(1,0,2,3)
-    
-    upper = (prediction * mask) * target
-    upper = 2 * torch.sum(upper)
+from torch.nn import functional as F
 
-    down = torch.sum((prediction * mask) **2) + torch.sum(target**2)
+def dice_loss(true, logits, ignore_background=False, eps=1e-7):
+    """Computes the Sørensen–Dice loss.
+    Note that PyTorch optimizers minimize a loss. In this
+    case, we would like to maximize the dice loss so we
+    return the negated dice loss.
+    Args:
+        true: a tensor of shape [B, 1, H, W].
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+        eps: added to the denominator for numerical stability.
+    Returns:
+        dice_loss: the Sørensen–Dice loss.
+    """
+    num_classes = logits.shape[1]
+    true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+    true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+    probas = F.softmax(logits, dim=1)
+        
+    true_1_hot = true_1_hot.type(logits.type())
+    dims = (0,) + tuple(range(2, true.ndimension()))
 
-    loss = (1 - (upper / down)) / batch_size
+    intersection = torch.sum(probas * true_1_hot, dims)
+    cardinality = torch.sum(probas + true_1_hot, dims)
 
-    return loss
+    if ignore_background:
+        dice_loss = (2. * intersection[1:] / (cardinality[1:] + eps)).mean()
+    else:
+        dice_loss = (2. * intersection / (cardinality + eps)).mean()
+    return (1 - dice_loss)
